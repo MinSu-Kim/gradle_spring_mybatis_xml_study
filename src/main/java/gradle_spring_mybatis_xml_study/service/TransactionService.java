18da13f8e4a74dfaf8c1db2edff88e0bba6f797f
teacher <net94.teacher@gmail.com>
2020-09-29 23:45:38 +0900

    코드 정리

--- src/main/java/gradle_spring_mybatis_xml_study/service/TransactionService.java
+++ src/main/java/gradle_spring_mybatis_xml_study/service/TransactionService.java
@@ -1,7 +1,5 @@
 package gradle_spring_mybatis_xml_study.service;
 
-import org.springframework.stereotype.Service;
-
 import gradle_spring_mybatis_xml_study.dto.Department;
 import gradle_spring_mybatis_xml_study.dto.Employee;
 
